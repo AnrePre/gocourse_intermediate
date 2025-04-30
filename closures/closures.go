daaf955a4771284f2e7a0ced1786ff0a75f4a5e5
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-04-30 16:42:12 +0200

    version 1 of closures

--- closures/closures.go
+++ closures/closures.go
@@ -1,5 +1,23 @@
 package main
 
+
+import "fmt"
+
+
 func main() {
-	//This is a little test
+	sequence:= adder()
+	fmt.Println(sequence())
+
+}
+
+func adder () func() int {
+	i := 0
+	fmt.Println("Previous value of i:", i)
+	return func() int{
+		i++
+		fmt.Println("added 1 to i")
+		return i
+	}
+	
+
 }
