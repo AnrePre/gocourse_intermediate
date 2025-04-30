19958b7eb307b1fd287a4d082fb9ddf391790e80
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-04-30 17:46:37 +0200

    finished closures

--- closures/closures.go
+++ closures/closures.go
@@ -5,8 +5,31 @@ import "fmt"
 
 
 func main() {
-	sequence:= adder()
+	//TEST 1
+	sequence:= adder() //since the function value is only returned here (which is another function value), the i:=0 does not
+			   //happern again, meaning that i value does not get reset. and the previous i value is stored in memory. 
 	fmt.Println(sequence())
+	fmt.Println(sequence())
+	fmt.Println(sequence())
+	fmt.Println(sequence())
+
+	sequence2:= adder()
+	fmt.Println(sequence2())
+	
+	//TEST 2
+	subtracter := func() func(int) int{
+		countdown:= 99			//we are keeping the countdown altered value in memory. Every time we run this.
+		return func(x int) int{
+			countdown -= x
+			return countdown
+		}
+	}()
+
+	fmt.Println(subtracter(1))
+	fmt.Println(subtracter(1))
+	fmt.Println(subtracter(1))
+	fmt.Println(subtracter(1))
+	fmt.Println(subtracter(1))
 
 }
 
