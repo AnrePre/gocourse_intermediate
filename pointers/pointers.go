1d8c8d30c626113e50fc5e056bdeda93319e1bd7
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-05-01 10:02:10 +0200

    pointers v1

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -2,6 +2,8 @@
 //referencing $ and dereferencing *
 //allow us to modify the value of a variable indirectly
 //var ptr *int (declaring the pointer)
+//there is no pointer arithmatic in go, unlike c
+
 
 package main
 
@@ -11,9 +13,27 @@ func main() {
 	
 	var ptr *int
 	var a int = 10
-	ptr = &a
-
+	ptr = &a //referecing a pointer
+		
 
 	fmt.Println(a)
 	fmt.Println(ptr)
+	fmt.Println(&a)	  //same as ptr
+	fmt.Println(*ptr) //dereference the pointer
+	//a pointer that is not popointing to any value is a nil pointer (when no value is declared)
+	if ptr == nil {
+		fmt.Println("Pointer is nil")
+	}
+
+
+	modifyValue(ptr)
+	fmt.Println(a)
+}
+
+
+
+
+func modifyValue(ptr *int) {	//when we pass a variable to a function, a copy of that variable is passed to the func.
+	*ptr++			//but when we are using pointers, we are accessing the memory address of the var
+				//and the value is modified. Thus no copy. :)
 }
