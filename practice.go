6bff2dfad1019060eeb55c964332526d62db8675
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-05-08 20:35:44 +0200

    hello world in practice.go file

--- practice.go
+++ practice.go
@@ -1,3 +1,6 @@
+// This program is made to practice and incorporate everything I have learnt in the intermediate section.
+//Use closures, recursion, pointers, strings and runes, formatting verbs, fmt package, structs, methods, interfaces, struct embedding, generics
+
 package main
 
 import "fmt"
