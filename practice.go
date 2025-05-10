a1c20102eae5c4dbd4b954f7fdc97ae35457db9a
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-05-10 08:27:34 +0200

    practice.go added printPointer

--- practice.go
+++ practice.go
@@ -70,9 +70,33 @@ loop:
 			//Pointers
 			fmt.Println("---\nPointers")
 			fmt.Println("Pointers in Go are variables that store the memory address of another variable, allowing functions to modify values directly.")
+			printPointer := `
+			func main() {
+	
+			var ptr *int
+			var a int = 10
+			ptr = &a //referecing a pointer
+				
+			fmt.Println(a)
+			fmt.Println(ptr)
+			fmt.Println(&a)	  //same as ptr
+			fmt.Println(*ptr) //dereference the pointer
+			//a pointer that is not popointing to any value is a nil pointer (when no value is declared)
+			if ptr == nil {
+				fmt.Println("Pointer is nil")
+			}
 
+			modifyValue(ptr)
+			fmt.Println(a)
+			}
+
+			func modifyValue(ptr *int) {	//when we pass a variable to a function, a copy of that variable is passed to the func.
+				*ptr++			//but when we are using pointers, we are accessing the memory address of the var
+							//and the value is modified. Thus no copy. :)
+			}`
+			fmt.Println(printPointer)
 		case 4:
-			fmt.Println("Revising ")
+			fmt.Println("---\nStrings and Runes")
 
 		default:
 			fmt.Println("Exit")
