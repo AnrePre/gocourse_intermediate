44f615a04152e23b9a620ee81ddfcbb53fb174e8
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-05-12 18:22:44 +0200

    refactoring structure and sections of practice.go

--- practice.go
+++ practice.go
@@ -8,7 +8,35 @@ func main() {
 	fmt.Println(".\n.\n.\nWelcome to My Golang Revision Tool.")
 	fmt.Println("Last updated 05/09/2025")
 	//Intro
-loop:
+
+	//Select Basics/Intermediate/Advanced
+mainLoop:
+	for {
+		fmt.Println("Please Select 1.Basics or 2.Intermediate or 3.Advanced or 4.Exit")
+		var mode int
+		fmt.Scan(&mode)
+		switch mode {
+		case 1:
+			fmt.Println("You have selected Basics")
+
+		case 2:
+			fmt.Println("You have selected Intermediate")
+			intermediate()
+		case 3:
+			fmt.Println("You have selected Advanced")
+
+		case 4:
+			fmt.Println("You have selected Exit")
+			break mainLoop
+		default:
+			fmt.Println("You have selected an invalid value, exiting...")
+			break mainLoop
+		}
+	}
+}
+
+func intermediate() {
+intermediateLoop:
 	for {
 		fmt.Println("\n---\nPlease select the chapter that you would like to revisit")
 		fmt.Print("1. Closures\n2. Recursion\n3. Pointers\nYour answer: ")
@@ -21,86 +49,23 @@ loop:
 			//closures
 			fmt.Println("---\nClosures")
 			fmt.Println("Definition: A closure in Go is a function that captures and uses variables from its surrounding scope, even after that scope has exited.")
-			printClosure := `
-			func main() {
-				counter := closureFunc()
-				fmt.Println(counter())
-				fmt.Println(counter())
-				fmt.Println(counter())
-				fmt.Println(counter())	
-			}
-
-			func closureFunc() func() int {
-				count := 0
-				return func() int {
-					count++
-					return count
-				}
-			}
-
-			Result:`
-			fmt.Println(printClosure)
-			counter := closureFunc()
-			fmt.Println(counter())
-			fmt.Println(counter())
-			fmt.Println(counter())
-			fmt.Println(counter())
 		case 2:
 			//recursion
 			fmt.Println("---\nRecursion")
 			fmt.Println("Recursion in Go is when a function calls itself to solve a problem by breaking it down into smaller sub-problems, usually with a base case to stop the calls.")
-			printRecursion := `
-			// Recursion function of n + (n-1)
-
-			func main() {
-				fmtPrintln(recursionFunc(10))
-			}
-
-			func recursionFunc(n int) int {
-				if n < 1 {
-					return n
-				}
-				return n + recursionFunc(n-1)
-			}
-
-			Result:`
-			fmt.Println(printRecursion)
-			fmt.Println(recursionFunc(10))
 		case 3:
 			//Pointers
 			fmt.Println("---\nPointers")
 			fmt.Println("Pointers in Go are variables that store the memory address of another variable, allowing functions to modify values directly.")
-			printPointer := `
-			func main() {
-	
-			var ptr *int
-			var a int = 10
-			ptr = &a //referecing a pointer
-				
-			fmt.Println(a)
-			fmt.Println(ptr)
-			fmt.Println(&a)	  //same as ptr
-			fmt.Println(*ptr) //dereference the pointer
-			//a pointer that is not popointing to any value is a nil pointer (when no value is declared)
-			if ptr == nil {
-				fmt.Println("Pointer is nil")
-			}
-
-			modifyValue(ptr)
-			fmt.Println(a)
-			}
-
-			func modifyValue(ptr *int) {	//when we pass a variable to a function, a copy of that variable is passed to the func.
-				*ptr++			//but when we are using pointers, we are accessing the memory address of the var
-							//and the value is modified. Thus no copy. :)
-			}`
-			fmt.Println(printPointer)
 		case 4:
 			fmt.Println("---\nStrings and Runes")
 
+		case 5:
+			fmt.Println()
+
 		default:
 			fmt.Println("Exit")
-			break loop
+			break intermediateLoop
 		}
 		//Check if user wants to run the program agian or exit.
 		var input string
@@ -115,7 +80,6 @@ loop:
 		} else {
 			fmt.Println("Invalid input, please enter 'y' or 'n'")
 		}
-
 	}
 }
 
