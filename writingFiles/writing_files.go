4d62b4bae61609dcafb7de20785a3b8bda351349
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-05-13 17:36:30 +0200

    more write to files

--- writingFiles/writing_files.go
+++ writingFiles/writing_files.go
@@ -24,4 +24,17 @@ func main() {
 
 	fmt.Println("Data has been written to file successfully.")
 
+	file, err = os.Create("writeString.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("Hello Go!\n")
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Println("Writing to writeString.txt complete")
 }
