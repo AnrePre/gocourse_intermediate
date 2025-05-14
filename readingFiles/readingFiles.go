af090a4b86c91cb2164dc6abd818cbb35e7b47cf
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-05-14 17:20:43 +0200

    reading files completed

--- readingFiles/readingFiles.go
+++ readingFiles/readingFiles.go
@@ -1,30 +1,47 @@
-package main
+package intermediate
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
+
 	file, err := os.Open("output.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
 	defer func() {
-		fmt.Println("Closing opened file.")
+		fmt.Println("Closing open file")
 		file.Close()
 	}()
+
 	fmt.Println("File opened successfully.")
 
-	// Read the contents of the file
-	data := make([]byte, 1024) // Buffer to read data
-	_, err = file.Read(data)
-	if err != nil {
-		fmt.Println("Error reading data from file:", err)
-		return
+	// // Read the contents of the opened file
+	// data := make([]byte, 1024) // Buffer to read data into
+	// _, err = file.Read(data)
+	// if err != nil {
+	// 	fmt.Println("Error reading data from file:", err)
+	// 	return
+	// }
+
+	// fmt.Println("File content:", string(data))
+
+	scanner := bufio.NewScanner(file)
+
+	// Read line by line
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println("Line:", line)
 	}
 
-	fmt.Println("File content:", string(data))
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
 
 }
