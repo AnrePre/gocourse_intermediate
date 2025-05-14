86a3237f2ed534230d85becb84f07422ccc34b0e
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-05-14 17:18:29 +0200

    readingFiles version 1

--- readingFiles/readingFiles.go
+++ readingFiles/readingFiles.go
@@ -1,7 +1,30 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	file, err := os.Open("output.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer func() {
+		fmt.Println("Closing opened file.")
+		file.Close()
+	}()
+	fmt.Println("File opened successfully.")
+
+	// Read the contents of the file
+	data := make([]byte, 1024) // Buffer to read data
+	_, err = file.Read(data)
+	if err != nil {
+		fmt.Println("Error reading data from file:", err)
+		return
+	}
+
+	fmt.Println("File content:", string(data))
 
 }
